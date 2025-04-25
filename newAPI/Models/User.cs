fd3f472b04e36b5780cf31bce4dc8cb16a0c4bd2
Alan Velazquez <bzbxlogan@gmail.com>
2025-04-25 11:04:15 -0700

    remade api

---
+++ newAPI/Models/User.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace RealEstate.Models
+
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string Role { get; set; } // "Realtor" or "Customer"
+
+        public string Password { get; set; }
+    }
+
+}
---
+++ newAPI/Program.cs
@@ -0,0 +1,30 @@
+using System;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services
+builder.Services.AddControllers();
+
+// Allow CORS for local frontend testing
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll",
+        policy => policy.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod());
+});
+
+var app = builder.Build();
+
+app.UseHttpsRedirection();
+
+app.UseCors("AllowAll");
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
+
